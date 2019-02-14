df1ccc775c43e6c50847158b4a420f0148b68fa9
aarkwright <apollo.arkwright@gmail.com>
2019-02-14 14:26:07 +0200

    Added Flask demo project. Various changes.

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from esipy import EsiClient
 from esipy import EsiSecurity
 from esipy.exceptions import APIException
 
+from flask import Flask
+
 
 config = configparser.ConfigParser()
 config.read('pyThia.cfg')
@@ -18,21 +20,22 @@ config.read('pyThia.cfg')
 # ESIPY Init
 # -----------------------------------------------------------------------
 # create the app
-esiapp = EsiApp().get_latest_swagger
+esiApp = EsiApp()
+# esiCurrent = esiApp.get_latest_swagger()
 
 # init the security object
-esisecurity = EsiSecurity(
-    redirect_uri=config['global']['ESI_CALLBACK'],
-    client_id=config['global']['ESI_CLIENT_ID'],
-    secret_key=config['global']['ESI_SECRET_KEY'],
-    headers={'User-Agent': config['global']['ESI_USER_AGENT']}
+esiSecurity = EsiSecurity(
+    redirect_uri=config['ESI']['CALLBACK'],
+    client_id=config['ESI']['CLIENT_ID'],
+    secret_key=config['ESI']['SECRET_KEY'],
+    headers={'User-Agent': config['ESI']['USER_AGENT']}
 )
 
 # init the client
-esiclient = EsiClient(
-    security=esisecurity,
+esiClient = EsiClient(
+    security=esiSecurity,
     cache=None,
-    headers={'User-Agent': config['global']['ESI_USER_AGENT']}
+    headers={'User-Agent': config['ESI']['USER_AGENT']}
 )
 
 
@@ -45,10 +48,10 @@ def generate_token():
     rand = random.SystemRandom()
     random_string = ''.join(rand.choice(chars) for _ in range(40))
     return hmac.new(
-        config['global']['SECRET_KEY'].encode('utf-8'),
+        config['ESI']['SECRET_KEY'].encode('utf-8'),
         random_string.encode('utf-8'),
         hashlib.sha256
     ).hexdigest()
 
 token = generate_token()
-esisecurity.get_auth_uri(state=token, scopes=['esi-wallet.read_character_wallet.v1'])
+esiSecurity.get_auth_uri(state=token, scopes=['esi-wallet.read_character_wallet.v1'])
